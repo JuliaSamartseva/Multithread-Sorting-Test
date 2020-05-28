f125799b3d028990eb1ea3cd67d8faaf4d4f035b
Julia0212 <yulia.samartseva@gmail.com>
2020-05-28 22:37:01 +0300

    Add time analysis for multithread merge sort

--- Multithread test/Test.cpp
+++ Multithread test/Test.cpp
@@ -1,6 +1,10 @@
 #define BOOST_TEST_MODULE mytests
 #include <boost/test/included/unit_test.hpp>
 #include "SortingAlgorithms.h"
+#include <iostream>
+#include <chrono>
+#include <execution>
+
 
 std::vector<int> generateArray(int size) {
     std::vector<int> result;
@@ -23,12 +27,34 @@ BOOST_AUTO_TEST_CASE(check_sorting)
 
 BOOST_AUTO_TEST_CASE(check_same_results)
 {
-    std::vector<int> x = generateArray(20480);
-    std::vector<int> y = x;
-    SortingContext context;
-    context.setStrategy(new MultithreadMergeSort());
-    context.sortNumbers(x);
-    context.setStrategy(new SequentialMergeSort());
-    context.sortNumbers(y);
-    BOOST_CHECK(x == y);
+    std::cout << "From this test case we can see that the 4 threads merge sort duration is\non average 2 times faster than sequential one";
+    int array_size = 32;
+    for (int i = 0; i < 8; i++) {
+        
+        std::vector<int> x = generateArray(array_size);
+        std::vector<int> y = x;
+        SortingContext context;
+
+        std::cout << "\n--------------------------------" << "Array size: " << array_size << std::endl;
+
+        context.setStrategy(new MultithreadMergeSort());
+        auto t1_thread = std::chrono::high_resolution_clock::now();
+        context.sortNumbers(x);
+        auto t2_thread = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2_thread - t1_thread).count();
+        std::cout << "Merge sort multithread duration: " << duration << std::endl;
+
+        context.setStrategy(new SequentialMergeSort());
+        auto t1_sequential = std::chrono::high_resolution_clock::now();
+        context.sortNumbers(y);
+        auto t2_sequential = std::chrono::high_resolution_clock::now();
+        auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(t2_sequential - t1_sequential).count();
+        std::cout << "Merge sort sequential duration: " << duration2 << std::endl;
+
+        array_size *= 4;
+
+        BOOST_CHECK(x == y);
+    }
+    
 }
+
